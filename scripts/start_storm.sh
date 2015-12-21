a6eaad481160ed4e39a647fcbaabe1947b85bb25
gbraccialli <gbraccialli@gmail.com>
2015-12-21 17:41:13 -0200

    update path

--- scripts/start_storm.sh
+++ scripts/start_storm.sh
@@ -1,1 +1,2 @@
-storm jar /root/cdr/telco-cdr-monitoring-1.0-SNAPSHOT.jar com.github.gbraccialli.telco.cdr.storm.Topology /root/cdr/conf/topology.props
+storm jar /root/telco-cdr-monitoring/target/telco-cdr-monitoring-1.0-SNAPSHOT.jar com.github.gbraccialli.telco.cdr.storm.Topology /root/telco-cdr-monitoring/conf/topology.props
+
