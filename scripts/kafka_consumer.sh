a5f17abb45f76c81b1f167faab46f9d532966da8
gbraccialli <gbraccialli@gmail.com>
2015-12-22 10:16:00 -0200

    fix path kafka bin

--- scripts/kafka_consumer.sh
+++ scripts/kafka_consumer.sh
@@ -1,1 +1,1 @@
-/usr/hdp/2.2.4.2-2/kafka/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic cdr --from-beginning
+/usr/hdp/current/kafka-broker/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic cdr --from-beginning
--- scripts/kafka_producer.sh
+++ scripts/kafka_producer.sh
@@ -1,1 +1,1 @@
-/usr/hdp/2.2.4.2-2/kafka/bin/kafka-console-producer.sh --broker-list sandbox.hortonworks.com:6667 --topic cdr
+/usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list sandbox.hortonworks.com:6667 --topic cdr
