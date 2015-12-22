8cd6db9eee79433d4d9c46bf0f2412aa9a0b1a41
gbraccialli <gbraccialli@gmail.com>
2015-12-22 12:38:53 -0200

    sleep

--- scripts/setup_solr.sh
+++ scripts/setup_solr.sh
@@ -8,6 +8,8 @@ chown -R solr:solr /opt/lucidworks-hdpsearch/solr
 
 sudo -u solr /opt/lucidworks-hdpsearch/solr/bin/solr start -c -z localhost:2181
 
+sleep 5
+
 sudo -u solr /opt/lucidworks-hdpsearch/solr/bin/solr create -c cdr \
    -d data_driven_schema_configs \
    -s 1 \
